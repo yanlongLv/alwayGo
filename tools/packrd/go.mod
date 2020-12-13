c82eb5e9add5028f955d9f9a3f846a497e632df8
yanlonglv <lyl_office@163.com>
2020-12-13 20:03:53 +0800

    new project create other file

---
+++ tools/packrd/go.mod
@@ -0,0 +1,16 @@
+module github.com/alwayGo
+
+go 1.13
+
+require (
+	github.com/gobuffalo/packr/v2 v2.8.1
+	github.com/karrick/godirwalk v1.16.1 // indirect
+	github.com/rogpeppe/go-internal v1.6.2 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ tools/packrd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/alwayGo/pkg"
+)
+
+// /Users/yanlonglv/go/bin/packr2
+func main() {
+	app := cli.NewApp()
+	app.Name = "create new project"
+	app.Usage = "init project and product project directory"
+	app.Version = "1.0.0"
+	app.Commands = []*cli.Command{
+		{
+			Name:    "new",
+			Aliases: []string{"n"},
+			Usage:   "create new project and product new directory",
+			Action:  newProject,
+		}, {
+			Name:    "build",
+			Aliases: []string{"b"},
+			Usage:   "build project",
+			Action:  build,
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func newProject(ctx *cli.Context) error {
+	projectName := ctx.Args().Slice()[0]
+	pwd, _ := os.Getwd()
+	projectfile := path.Join(pwd, projectName)
+	return pkg.Create(projectfile,projectName)
+}
+
+func build(ctx *cli.Context) error {
+	fmt.Print("you are build")
+	return nil
+}
